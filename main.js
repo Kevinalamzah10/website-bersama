f912d7221b3d371b5aa001794ff0dceb41268201
dikaarrohman <dlanggudika@gmail.com>
2024-08-28 02:08:43 -0700

    asu

---
+++ main.js
@@ -0,0 +1,15 @@
+// Toggle & Responsive Navigation
+const navSlide = () =>{
+    conts burger = document.querySelector(".burger")
+    conts navLists = document.querySelector("nav")
+
+    
+}
+
+// Clear form before uunload
+window.onbeforeundload = () =>{
+    for (conts form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
+
