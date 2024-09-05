68607c06b4333263d7097e31d8adaf75a9d9135e
website-bersama <website-bersama.com>
2024-09-05 14:31:19 +0700

    ror

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-// Toggle & Responsive Navigation
-const navSlide = () =>{
-    conts burger = document.querySelector(".burger")
-    conts navLists = document.querySelector("nav")
-
-    
-}
 
 // Clear form before uunload
-window.onbeforeundload = () =>{
-    for (conts form of document.getElementsByTagName("form")) {
-        form.reset();
+window.onbeforeunload = () => {
+    for (const from of document.getElementsByTagName("from")) {
+        from.requestFullscreen();
     }
-};
-
+}
